e614cd8877363516e09e4f47142ef19abc533616
Colemorrison <colemorrison321@icloud.com>
2024-09-18 19:51:58 -0400

    Added budget rep and added test cases in main function.

--- src/main/java/edu/fscj/cen3024c/financialclarity/FinancialClarityApplication.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/FinancialClarityApplication.java
@@ -4,12 +4,17 @@ package edu.fscj.cen3024c.financialclarity;
 import edu.fscj.cen3024c.financialclarity.entity.Users;
 import edu.fscj.cen3024c.financialclarity.repository.UsersRepository;
 
+import edu.fscj.cen3024c.financialclarity.entity.Budget;
+import edu.fscj.cen3024c.financialclarity.repository.BudgetRepository;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Date;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,6 +25,9 @@ public class FinancialClarityApplication implements CommandLineRunner {
 	@Autowired
 	private UsersRepository usersRepository;
 
+	@Autowired
+	private BudgetRepository budgetRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialClarityApplication.class, args);
 	}
@@ -27,7 +35,7 @@ public class FinancialClarityApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-
+		// Create User
 		Users user1 = new Users();
 		user1.setUsername("Cole");
 		user1.setAge(20);
@@ -37,13 +45,27 @@ public class FinancialClarityApplication implements CommandLineRunner {
 		user1.setTotalExpences("1000");
 		usersRepository.save(user1);
 
+		// Create Budget
+		Budget budget = new Budget();
+		budget.setUserId(user1.getId()); // Set the userId to the id of the saved user
+		budget.setBudgetName("Monthly Budget");
+		budget.setTimeCreate(new Date()); // Current date and time
+		budgetRepository.save(budget);
 
 
-		// Call the findAll() method to test it
+
+
+
+		// Print all Users
 		List<Users> users = usersRepository.findAll();
-
-		// Print the results to verify
 		System.out.println("All users in the database:");
 		users.forEach(user -> System.out.println(user.getUsername()));
+
+
+		// Print all Budgets
+		List<Budget> budgets = budgetRepository.findAll();
+		System.out.println("All Budgets in the database:");
+		budgets.forEach(b -> System.out.println(b.getBudgetName()));
+
 	}
 }
