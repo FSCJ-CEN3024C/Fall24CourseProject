dadd10ea8087af1bcb2431bc2d3ae9c5f8aa33a6
Dayne Wright <alledr6@students.fscj.edu>
2024-09-18 21:24:20 -0400

    Added Income and Expenses

--- src/main/java/edu/fscj/cen3024c/financialclarity/FinancialClarityApplication.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/FinancialClarityApplication.java
@@ -1,7 +1,11 @@
 package edu.fscj.cen3024c.financialclarity;
 
 // Entity and repostirory imports
+import edu.fscj.cen3024c.financialclarity.entity.Expenses;
+import edu.fscj.cen3024c.financialclarity.entity.Income;
 import edu.fscj.cen3024c.financialclarity.entity.Users;
+import edu.fscj.cen3024c.financialclarity.repository.ExpensesRepository;
+import edu.fscj.cen3024c.financialclarity.repository.IncomeRepository;
 import edu.fscj.cen3024c.financialclarity.repository.UsersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,10 @@ public class FinancialClarityApplication implements CommandLineRunner {
 
 	@Autowired
 	private UsersRepository usersRepository;
+	@Autowired
+	private IncomeRepository incomeRepository;
+	@Autowired
+	private ExpensesRepository expensesRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialClarityApplication.class, args);
@@ -37,13 +45,31 @@ public class FinancialClarityApplication implements CommandLineRunner {
 		user1.setTotalExpences("1000");
 		usersRepository.save(user1);
 
+		Income income1 = new Income();
+		income1.setName("Paycheck 1");
+		income1.setAmount(1200.00f);
+		incomeRepository.save(income1);
+
+		Expenses expense1 = new Expenses();
+		expense1.setName("Netflix");
+		expense1.setAmount(7.99f);
+		expensesRepository.save(expense1);
+
 
 
 		// Call the findAll() method to test it
 		List<Users> users = usersRepository.findAll();
+		List<Expenses> expenses = expensesRepository.findAll();
+		List<Income> incomes = incomeRepository.findAll();
 
 		// Print the results to verify
 		System.out.println("All users in the database:");
 		users.forEach(user -> System.out.println(user.getUsername()));
+
+		System.out.println("All expenses in the database:");
+		expenses.forEach(expense -> System.out.println(expense.getName() + " " + expense.getAmount()));
+
+		System.out.println("All incomes in the database:");
+		incomes.forEach(income -> System.out.println(income.getName() + " " + income.getAmount()));
 	}
 }
--- src/main/java/edu/fscj/cen3024c/financialclarity/repository/IncomeRepository.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/repository/IncomeRepository.java
@@ -1,12 +1,13 @@
 package edu.fscj.cen3024c.financialclarity.repository;
-import edu.fscj.cen3024c.financialclarity.entity.Users;
+
+import edu.fscj.cen3024c.financialclarity.entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-    // Find all Users
-    List<Users> findAll();
+public interface IncomeRepository extends JpaRepository<Income, Integer> {
+    // Find all Incomes
+    List<Income> findAll();
 }
