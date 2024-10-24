558e01217ed849e8b41b1238addda0dabdfbe81d
Smith <S3731317@students.fscj.edu>
2024-10-23 20:58:16 -0400

    added Expense Controller please finish it and ExpenseService

--- src/main/java/edu/fscj/cen3024c/financialclarity/controller/ExpensesController.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/controller/ExpensesController.java
@@ -1,6 +1,8 @@
 package edu.fscj.cen3024c.financialclarity.controller;
 
 
+import edu.fscj.cen3024c.financialclarity.dto.ExpensesDTO;
+import edu.fscj.cen3024c.financialclarity.dto.UserDTO;
 import edu.fscj.cen3024c.financialclarity.entity.Expenses;
 import edu.fscj.cen3024c.financialclarity.entity.User;
 import edu.fscj.cen3024c.financialclarity.service.ExpenseService;
@@ -8,14 +10,28 @@ import edu.fscj.cen3024c.financialclarity.service.ExpenseService;
 
 import edu.fscj.cen3024c.financialclarity.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/expenses")
 public class ExpensesController {
+
     @Autowired
     private ExpenseService expenseService;
+
+    private ExpensesDTO convertToDTO(Expenses expenses) {
+        return new ExpensesDTO(expenses.getExpenseId(), expenses.getUserId(), expenses.getAmount(), expenses.getName());
+    }
+    @GetMapping
+    public List<ExpensesDTO> getAllUsers() {
+        List<Expenses> expenses = expenseService.findAll();
+        return expenses.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
 
     @CrossOrigin(origins = {"http://example.com", "http://localhost"})
     @GetMapping("/{expensesId}")
@@ -23,6 +39,12 @@ public class ExpensesController {
 
         Expenses expenses = expenseService.findByExpencesId(expensesId);
         return ResponseEntity.ok(expenses);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ExpensesDTO> createUser(@RequestBody ExpensesDTO expensesDTO) {
+        ExpensesDTO savedExpense = expenseService.save(ExpensesDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
 
 }
--- src/main/java/edu/fscj/cen3024c/financialclarity/service/ExpenseService.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/service/ExpenseService.java
@@ -1,10 +1,15 @@
 package edu.fscj.cen3024c.financialclarity.service;
 
+import edu.fscj.cen3024c.financialclarity.dto.ExpensesDTO;
+import edu.fscj.cen3024c.financialclarity.dto.UserDTO;
 import edu.fscj.cen3024c.financialclarity.entity.Expenses;
 
+import edu.fscj.cen3024c.financialclarity.entity.User;
 import edu.fscj.cen3024c.financialclarity.repository.ExpensesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExpenseService {
@@ -12,4 +17,21 @@ public class ExpenseService {
     @Autowired
     private ExpensesRepository expensesRepository;
     public Expenses findByExpencesId(Integer expensesId) {return expensesRepository.findByExpenseId(expensesId);}
+    public List<Expenses> findAll() {return expensesRepository.findAll();}
+    public Expenses save(Expenses expenses ) { return expensesRepository.save(expenses); }
+    public ExpensesDTO save(ExpensesDTO expensesDTO) {
+        // Convert ExpenseDTO to Expense entity
+        Expenses expenses = new Expenses();
+        expenses.setExpenseId(ExpensesDTO.getExpenseId());
+        expenses.setUserId(ExpensesDTO.getUserId());
+        expenses.setAmount(ExpensesDTO.getAmount());
+        expenses.setName(ExpensesDTO.getName());
+
+        // Save the User entity
+        Expenses savedExpenses = expensesRepository.save(expenses);
+
+        // Convert saved User entity back to UserDTO and return
+        return convertToDTO(savedExpenses);
+    }
 }
+
