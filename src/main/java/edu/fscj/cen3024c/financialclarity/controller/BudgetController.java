d80add66194063dfec50056750f56fc154656e9a
Colemorrison <colemorrison321@icloud.com>
2024-10-30 16:04:59 -0400

    adding logging and profiler to endpoints budget and user

--- src/main/java/edu/fscj/cen3024c/financialclarity/controller/BudgetController.java
+++ src/main/java/edu/fscj/cen3024c/financialclarity/controller/BudgetController.java
@@ -14,14 +14,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.profiler.Profiler;
+import org.slf4j.profiler.TimeInstrument;
+
+
 @RestController
 @RequestMapping("/budget")
 public class BudgetController {
 
     @Autowired
     private BudgetService budgetService;
-    @Autowired
-    private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(BudgetController.class);
 
     private BudgetDTO convertToDTO(Budget budget) {
         return new BudgetDTO(budget.getId(), budget.getUserId(), budget.getBudgetName(), budget.getTimeCreate());
@@ -32,6 +39,7 @@ public class BudgetController {
     @GetMapping
     public List<BudgetDTO> getAllBudgets() {
         List<Budget> budgets = budgetService.findAll();
+        logger.error("Found all budgets");
         return budgets.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
@@ -39,12 +47,23 @@ public class BudgetController {
     public ResponseEntity<BudgetDTO> getBudgetById(@PathVariable String budgetName) {
         Budget budget = budgetService.findByBudgetName(budgetName);
         BudgetDTO budgetDTO = convertToDTO(budget);
+        logger.error("Found budget {}", budgetName);
         return ResponseEntity.ok(budgetDTO);
     }
 
     @PostMapping()
     public ResponseEntity<BudgetDTO> createBudget(@RequestBody BudgetDTO budgetDTO) {
+        // start the profiler
+        Profiler profiler = new Profiler("createUser");
+        profiler.start("Create User");
+
         BudgetDTO savedBudget = budgetService.save(budgetDTO);
+        logger.info("A new budget has been added: {}", savedBudget.getBudgetName());
+
+        // stop the profiler
+        TimeInstrument ti = profiler.stop();
+        ti.print();
+
         return ResponseEntity.status(HttpStatus.CREATED).body(savedBudget);
     }
 
@@ -55,8 +74,10 @@ public class BudgetController {
             budget.setBudgetName(budgetDTO.getBudgetName());
             budget.setUserId(budgetDTO.getUserId());
             Budget updatedBudget = budgetService.save(budget);
+            logger.info("A budget has been updated: {}", budgetName);
             return new ResponseEntity<>(convertToDTO(updatedBudget), HttpStatus.OK);
         } else {
+            logger.error("Update budget failed, not found: {}", budgetName);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -64,6 +85,7 @@ public class BudgetController {
     @DeleteMapping("{budgetName}")
     public ResponseEntity<BudgetDTO> deleteBudget(@PathVariable String budgetName) {
         budgetService.deleteByBudgetName(budgetName);
+        logger.error("Deleted budget {}", budgetName);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
